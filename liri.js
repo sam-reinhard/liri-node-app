5c8b0a8f114d38c21812895a3474fd328634b7a2
Sam Reinhard <SamReinhard@Sams-MacBook-Pro.local>
2019-03-29 16:06:22 -0500

    uploading working spotify, omdb, and bandsintown pieces

---
+++ liri.js
@@ -0,0 +1,134 @@
+//Things to do
+    //take spotify keys out of the main document
+    //finish conditionals on dowhatitsays function
+
+var dotenv = require("dotenv").config();
+var keys = require("./keys.js");
+var axios = require("axios");
+var moment = require("moment");
+var fs = require("fs");
+
+var Spotify = require("node-spotify-api");
+
+var spotify = new Spotify({
+    id: '2b51be9d610d4ee2ac3820d6a80c4c03',
+    secret: 'ab9220414eb140588aaacd8ef511221e'
+});
+
+var command = process.argv[2];
+
+//function for calling spotify
+function spotifyFunc(){
+    if (process.argv[3]){
+        var query = process.argv.slice(3);
+    }
+    else {
+        var query = "the sign ace of base";
+    }
+
+    // write artist(s), the song's name, preview link of the song, and if no song is provided default to "the sign" by ace of bass
+    spotify.search({type: 'track', query: query}).then(function(response){
+        
+        var songInfo = response.tracks.items[0];
+        console.log("\n" + songInfo.name);
+        console.log(songInfo.artists[0].name);
+        console.log(songInfo.album.name);
+        console.log(songInfo.preview_url + "\n");
+    });
+};
+
+
+//function for calling omdb
+function omdb(){
+    // if the user doesn't write anything, plug in Mr. Nobody
+    if (process.argv[3]){
+        var query = process.argv.slice(3).join("+");
+    } else {
+        var query = "mr+nobody";
+    }
+
+    var movieUrl = "http://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=trilogy";
+
+    axios.get(movieUrl).then(function(response){
+        // write title, year, imdb rating, rotten tomatoes score, country produced, language, plot, actors
+        console.log("\nTitle: " + response.data.Title);
+        console.log("Year: " + response.data.Year);
+        console.log("IMDB Rating: " + response.data.imdbRating);
+        console.log("Rotten Tomatoes Score: " + response.data.Ratings[1].Value);
+        console.log("Country Produced: " + response.data.Country);
+        console.log("Language: " + response.data.Language);
+        console.log("Plot: " + response.data.Plot);
+        console.log("Actors: " + response.data.Actors + "\n");
+    })
+};
+
+//function for calling bandsintown
+function bandsInTown(){
+    var query = process.argv.slice(3).join("+");
+    var bandUrl = "https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp";
+
+    axios.get(bandUrl).then(function(response){
+        //write the name of the venue / venue location / date of the concert for each concert in the object
+        for (var i = 0; i < response.data.length; i++){
+            console.log("\nVenue Name: " + response.data[i].venue.name);
+            if (response.data[i].venue.region){
+                console.log("Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region);
+            }
+            else {
+                console.log("Location: " + response.data[i].venue.city + ", " + response.data[i].venue.country); 
+            }
+
+            // convert date from object into right format using moment.js
+            var date = moment(response.data[i].datetime).format("MM/DD/YYYY");
+            console.log("Date: " + date + "\n");
+        }
+    })
+};
+
+//function for do what it says
+//maybe have two parameters(command, query) and use query for the variables?
+function doWhatItSays(command, query){
+    fs.readFile("random.txt", "utf8", function(error, response){
+        if (error) {
+            return console.log(error);
+        }
+
+        var responseArr = response.split(",");
+        command = responseArr[0];
+        query = responseArr[1];
+        followCommand(command, query);
+        return;
+        // if (command==="concert-this"){
+        //     //var artist = the second thing in response array, formatted for the url
+        //     bandsInTown();
+        // }
+        // else if (command="spotify-this-song"){
+        //     //var song = the second thing in response array, formatted for the url
+        //     spotify();
+        // }
+        // else if(command==="movie-this"){
+        //     //var movie = the second thing in response array, formatted for the url
+        //     omdb();
+        // }
+    })
+};
+
+
+// conditionals for accepting the user command
+//put this within a function
+function followCommand (){
+    if (command==="concert-this"){
+        bandsInTown();
+
+    } else if (command==="spotify-this-song"){
+        spotifyFunc();
+
+    } else if (command==="movie-this"){
+        omdb();
+
+    } else if (command==="do-what-it-says"){
+        doWhatItSays(command, query);
+    }
+};
+
+followCommand();
